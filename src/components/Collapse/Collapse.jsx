e1104802e8a76a808759a7fe8e035ae6346f23f7
Sandy D <sandyDpro@ik.me>
2024-07-12 13:01:34 +0200

    Mobile Home page

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
 // import "./../../assets/scss/style.scss";
-import './Collapse.scss'; 
+import './Collapse.scss';
+import IconOpen from "./../../assets/images/icons/arrow_up.png";
 
 const title = "Description";
+const toggleCollapsible = (e) => {
+  console.log(e,"J'ai cliqué sur IconOpen")
+}
 
 function Collapse() {
-  const [isOpen, setIsOpen] = useState(false);
-  
-  const toggleCollapsible = () => {
-    setIsOpen(!isOpen);
-  };
-  
   return (
+
     <>
       <button type="button" className="collapsible" onClick={toggleCollapsible}>
-        {title}
+        <p>{title}
+          <span><img src={IconOpen} alt="flèche du haut" /></span>
+        </p>
       </button>
-      <div className={`content ${isOpen ? 'open' : ''}`}>
+      <div className="content">
         <p>
           Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et
           restaurants.<br />Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les
@@ -72,3 +73,22 @@ export default Collapse;
 // }
 
 // export default Collapse;
+
+// const [isOpen, setIsOpen] = useState(false);
+// const toggleCollapsible = () => {
+//   setIsOpen(!isOpen);
+// };
+
+// <button type="button" className="collapsible" onClick={toggleCollapsible}>
+// {title}
+// </button>
+// <div className={`content ${isOpen ? 'open' : ''}`}>
+// <p>
+//   Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et
+//   restaurants.<br />Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les
+//   voyageurs en solo et les voyageurs d'affaires.<br />Vous êtes à 1 station de la gare de l'est (7 minutes à
+//   pied).
+// </p>
+// </div>
+// </>
+// );
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Banner from "./../../components/Banner/Banner";
-import BannerImage from "./../../assets/images/img/bannerAbout.jpg";
+import BannerImage from "./../../assets/images/img/bannerAbout.png";
 import Collapse from "./../../components/Collapse/Collapse";
 
 function About() {
