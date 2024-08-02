01cdc6a9d48d83a523b9cfe8a34e216400ab91d0
Sandy D <sandyDpro@ik.me>
2024-08-02 21:53:12 +0200

    Header style changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,12 @@ import "./Header.scss";
 function Header() {
     const location = useLocation();
     return (
-    <header>
-        <img src={Logo} alt="logo Kasa" />
-        <nav>
+    <header className="header">
+        <img src={Logo} alt="logo Kasa" className="logo"/>
+        <nav className="nav">
             <ul>
-                <li><NavLink to="/" className={location.pathname === '/' ? 'selected' : 'navlink'}>Accueil</NavLink></li>
+                <li><NavLink to="/" className={location.pathname === '/' && 'selected'}>Accueil</NavLink></li>
+                {/* <li><NavLink to="/" className={location.pathname === '/' ? 'selected' : 'navlink'}>Accueil</NavLink></li> */}
                 <li><NavLink to="/about" className={location.pathname === '/about' && 'selected'}>A propos</NavLink></li>
            </ul>
         </nav>
