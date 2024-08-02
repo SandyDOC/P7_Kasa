8d14a8baeb7bc48e539c61db2dcf6e87d6e82bab
Sandy D <sandyDpro@ik.me>
2024-08-02 23:05:18 +0200

    Header style changed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ import "./Banner.scss";
 function Banner({image, title}) {
     return (
         <div id="banner">
-            <img src={image} alt="bannière"/>
+            <img src={image} alt="bannière" className="bannerImg"/>
             <p>{title}</p>
         </div>
     )
