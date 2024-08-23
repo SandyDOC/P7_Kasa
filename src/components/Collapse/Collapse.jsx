9c0103457f3d3496991c502e73e8fe429eff5505
Sandy D <sandyDpro@ik.me>
2024-08-23 18:33:29 +0200

    Added Collapse content scroll transition effect

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import IconArrow from "./../../assets/images/icons/arrow_up.png";
 import './Collapse.scss';
 
 const Collapse = ({ title, content }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef();
 
   return (
     <div className="collapse">
@@ -15,8 +16,17 @@ const Collapse = ({ title, content }) => {
         {title}
         <img className={"icon " + (isOpen ? 'open' : '')} src={IconArrow} alt="triangle vers le haut" />
       </button>
-      <div className={"content " + (isOpen ? "display" : "")} >
-        {content}
+
+      <div ref={contentRef}
+        className='content-container'
+        style={isOpen ?
+          { height: contentRef.current.scrollHeight + "px", } : {
+            height: "0px",
+          }
+        }>
+        <div className="content">
+          {content}
+        </div>
       </div>
     </div>
   );
